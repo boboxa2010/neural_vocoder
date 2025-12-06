4505f91c1ac7901245e9996770fda95cb16bb9ba
Ivan Boboshko <ivanboboshko888@gmail.com>
2025-12-07 01:13:26 +0300

    fix bugs in losses again

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,4 +1,5 @@
 from src.loss.discriminator_loss import DiscriminatorLoss
 from src.loss.generator_loss import GeneratorLoss
+from src.loss.hifi_gan_loss import HiFiGANLoss
 
-__all__ = ["DiscriminatorLoss", "GeneratorLoss"]
+__all__ = ["DiscriminatorLoss", "GeneratorLoss", "HiFiGANLoss"]
