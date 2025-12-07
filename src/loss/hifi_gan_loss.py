ce482c0eaece614c3c948daaad65117071478d56
Ivan Boboshko <ivanboboshko888@gmail.com>
2025-12-08 01:53:55 +0300

    add norm params in loss

--- src/loss/hifi_gan_loss.py
+++ src/loss/hifi_gan_loss.py
@@ -1,12 +1,20 @@
 from torch import nn
 
-from src.loss.generator_loss import GeneratorLoss
 from src.loss.discriminator_loss import DiscriminatorLoss
+from src.loss.generator_loss import GeneratorLoss
+
 
 class HiFiGANLoss(nn.Module):
-    def __init__(self, feature_weight: float = 2.0, mel_weight: float = 45.0):
+    def __init__(
+        self,
+        feature_weight: float = 2.0,
+        mel_weight: float = 45.0,
+        normalize_adv: bool = True,
+        normalize_feature: bool = True,
+    ):
         super().__init__()
 
-        self.generator_loss = GeneratorLoss(feature_weight, mel_weight)
+        self.generator_loss = GeneratorLoss(
+            feature_weight, mel_weight, normalize_adv, normalize_feature
+        )
         self.discriminator_loss = DiscriminatorLoss()
-
